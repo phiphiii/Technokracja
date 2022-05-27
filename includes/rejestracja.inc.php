135417b0f3293a12b6ca3944e65b32f30869fe95
xkoza <filip.j.pietrzak@gmail.com>
2022-05-27 08:23:38 +0200

    Sign up page progress
    almost done

--- includes/rejestracja.inc.php
+++ includes/rejestracja.inc.php
@@ -22,12 +22,67 @@
             $pswrd2 = $_POST["password2"];
 
             if(empty($email) || empty($user) || empty($pswrd) || empty($pswrd2)){
-                header("Location ./rejestracja.php?error=emptyfields&email=".$email."&user=".$user);
+                header("Location: ../rejestracja.php?error=emptyfields&email=".$email."&user=".$user );
                 exit();
             }
+            else if(!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/",$user)){
+                header("Location ../rejestracja.php?error=invalidmailuser&user");
+                exit();
+            }
+            else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                header("Location ../rejestracja.php?error=invalidmail&user=".$user);
+                exit();
+            }
+            else if(!preg_match("/^[a-zA-Z0-9]*$/",$user)){
+                header("Location ../rejestracja.php?error=invaliduser&email=".$email);
+                exit();
+            }
+            else if($pswrd !== $pswrd2){
+                header("Location: ../rejestracja.php?error=passwordcheck&email=".$email."&user=".$user );
+                exit();
+            }
+            else{
+                $sql = "SELECT usernameUsers FROM users WHERE usernameUsers=?";
+                $stmt = mysqli_stmt_init($conn);
+                if(!mysqli_stmt_prepare($stmt, $sql)){
+                    header("Location: ../rejestracja.php?error=sqlerror");
+                    exit();
+
+                }
+                else{
+                     mysqli_stmt_bind_param($stmt, "s", $user);
+                     mysqli_stmt_execute($stmt);
+                     mysqli_stmt_store_result($stmt);
+                     $resultCheck = mysqli_stmt_num_rows($stmt);
+                     if($resultCheck>0){
+                        header("Location: ../rejestracja.php?error=usertaken&email=".$email);
+                        exit();
+                     }
+                     else{
+                        $sql = "INSERT INTO users (usernameUsers, emailUsers, passwordUsers) VALUES (?, ?, ?)";
+                        $stmt = mysqli_stmt_init($conn);
+                        if(!mysqli_stmt_prepare($stmt, $sql)){
+                            header("Location: ../rejestracja.php?error=sqlerror");
+                            exit();
+        
+                        }
+                        else{
+                            $hashedPswrd = password_hash($pswrd, PASSWORD_DEFAULT);
+                            mysqli_stmt_bind_param($stmt,"sss",$user,$email,$pswrd);
+                            mysqli_stmt_execute($stmt);
+                            header("Location: ../rejestracja.php?signup=success");
+                            exit();
+                        }
+                     }
+                }
+            }
+            mysqli_stmt_close($stmt);
+            mysqli_close($conn);
+        }
+        else{
+            header("Location: ../rejestracja.php");
+            exit();
         }
     }
     //echo $conn->host_info."\n";
-
-    $conn->close();
 ?>
